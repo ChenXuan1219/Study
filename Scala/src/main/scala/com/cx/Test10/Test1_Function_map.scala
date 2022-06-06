50f0635de1228d2441f10341e7f437af4c4b3a30
chenxuan <git@gitee.com:PinkmanChen/study.git>
2022-06-06 16:42:25 +0800

    第一次提交

---
+++ Scala/src/main/scala/com/cx/Test10/Test1_Function_map.scala
@@ -0,0 +1,12 @@
+package com.cx.Test10
+
+/**
+ * TODO
+ *
+ * @author chenxuan
+ * @version 1.0.0
+ * @since 2022/06/06 16:37
+ */
+class Test1_Function_map {
+
+}
---
+++ src/main/java/dwa.java
@@ -0,0 +1,9 @@
+/**
+ * TODO
+ *
+ * @author chenxuan
+ * @version 1.0.0
+ * @since 2022/06/06 16:39
+ */
+public class dwa {
+}
