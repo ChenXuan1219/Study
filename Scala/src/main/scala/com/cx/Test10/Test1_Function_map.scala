f7ec1c533582ad6cd74f50fb038298285f26c24c
chenxuan <git@gitee.com:PinkmanChen/study.git>
2022-06-06 17:00:13 +0800

    1

--- Scala/src/main/scala/com/cx/Test10/Test1_Function_map.scala
+++ Scala/src/main/scala/com/cx/Test10/Test1_Function_map.scala
@@ -12,5 +12,29 @@ package com.cx.Test10
 object test1 {
   def main(args : Array[String]): Unit ={
 
+    //1.过滤
+    val list = List(0,1,2,3)
+    val evenList = list.filter(_ % 2 == 0)
+
+    println(evenList)
+    println("======")
+    //2.每一个元素做操作
+    //把集合中每个元素乘2
+    println(list.map(_ * 2))
+    println(list.map(x => x * x))
+    println("====")
+
+    //3.扁平化
+    val nestedList:List[List[Int]] = List(List(1,2,3),List(4,5),List(6,7,8,9))
+
+    val flatList = nestedList(0) ::: nestedList(1) ::: nestedList(2)
+
+    println(nestedList)
+    println(flatList)
+
+    val flatList2 = nestedList.flatten
+    println(flatList2)
+
+
   }
 }
